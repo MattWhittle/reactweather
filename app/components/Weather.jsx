3f301cbe5644118a6f81938bf90613977415d618
MattWhittle <themwproject@yahoo.com>
2017-04-27 15:43:28 -0400

    added modal error message

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,28 +2,35 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
+var ErrorModal = require('ErrorModal');
 
 var Weather = React.createClass({
     getInitialState: function(){
         return {
             isLoading: false
+
         }
     },
     handleSearch: function(location){
         var that = this;
 
-        this.setState({isLoading:true});
+        this.setState({
+            isLoading:true,
+            errorMessage: undefined
+        });
 
         openWeatherMap.getTemp(location).then(function(weatherObject){
             that.setState(weatherObject);
             that.setState({isLoading:false});
-        }, function(errorMessage) {
-            that.setState({isLoading:false});
-            alert(errorMessage);
+        }, function(e) {
+            that.setState({
+                isLoading: false,
+                errorMessage: e.message
+        });
         });
     },
     render: function() {
-        var {isLoading, temp, location, conditions, iconId} = this.state;
+        var {isLoading, temp, location, conditions, iconId, errorMessage} = this.state;
 
         function renderMessage (){
             if(isLoading){
@@ -33,11 +40,20 @@ var Weather = React.createClass({
             }
         }
 
+        function renderError(){
+            if(typeof errorMessage === 'string') {
+                return (
+                    <ErrorModal message={errorMessage} />
+                );
+            }
+        }
+
         return (
             <div>
                 <h1 className="text-center">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch} />
                 {renderMessage()}
+                {renderError()}
             </div>
         );
     }
