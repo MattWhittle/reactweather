d12157cf08607a0d97b76711cc293bbdf546a42c
MattWhittle <themwproject@yahoo.com>
2017-04-27 10:53:51 -0400

    Added a little to About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,8 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the About page!</p>
+            <h1 className="text-center">About</h1>
+            <p>This is a weather application built with React.</p>
         </div>
     );
 };
