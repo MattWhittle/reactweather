7677855f231e56390c5b1f7e438ec0c549dff18a
MattWhittle <themwproject@yahoo.com>
2017-04-28 10:55:51 -0400

    Added content to the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,7 +12,15 @@ var About = (props) => {
     return (
         <div>
             <h1 className="text-center page-title">About</h1>
-            <p>This is a weather application built with React. To use: simply type any city name into the search(leave off the state) and hit the get weather button!</p>
+            <p>This is a weather application built with React.</p> 
+            <h4>To use:</h4> 
+            <p>Simply type any city name into the search(leave off the state) and hit the get weather button! You will receive the current location in the city you have searched.</p>
+            <h4>Technologies used:</h4>
+            <ol>
+                <li><a src="https://facebook.github.io/react/">ReactJS</a></li>
+                <li><a src="https://openweathermap.org/">OpenWeatherMap</a></li>
+                <li><a src="https://webpack.github.io/">Webpack</a></li>
+            </ol>
         </div>
     );
 };
